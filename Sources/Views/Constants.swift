798355b100c902d2cb6c2d455dbf59811caa7538
Eugene Tulushev <eugene@tulushev.com>
2020-11-23 21:15:23 +0200

    Mirror Color constants to UIColor

--- Sources/Views/Constants.swift
+++ Sources/Views/Constants.swift
@@ -22,9 +22,23 @@ public extension Color {
 }
 
 public extension UIColor {
+  static let almostWhite    =       #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+  static let cosmicLatte    =       #colorLiteral(red: 0.8784313725, green: 0.9764705882, blue: 0.9215686275, alpha: 1)
+  static let lilyWhite      =       #colorLiteral(red: 0.9529411765, green: 0.9607843137, blue: 0.9568627451, alpha: 1)
+  static let titanWhite     =       #colorLiteral(red: 0.8784313725, green: 0.8745098039, blue: 0.9019607843, alpha: 1)
+  static let ghost          =       #colorLiteral(red: 0.7803921569, green: 0.7764705882, blue: 0.8039215686, alpha: 1)
   static let greySuit       =       #colorLiteral(red: 0.5803921569, green: 0.5725490196, blue: 0.6156862745, alpha: 1)
+  static let topaz          =       #colorLiteral(red: 0.462745098, green: 0.4509803922, blue: 0.5058823529, alpha: 1)
   static let gunPowder      =       #colorLiteral(red: 0.2980392157, green: 0.2941176471, blue: 0.3647058824, alpha: 1)
   static let saltBox        =       #colorLiteral(red: 0.3450980392, green: 0.3411764706, blue: 0.3450980392, alpha: 1)
+  static let haiti          =       #colorLiteral(red: 0.1450980392, green: 0.1411764706, blue: 0.1843137255, alpha: 1)
+  static let darkPink       =       #colorLiteral(red: 0.9254901961, green: 0.2509803922, blue: 0.4784313725, alpha: 1)
+  static let radicalRed     =       #colorLiteral(red: 0.9960784314, green: 0.3019607843, blue: 0.3725490196, alpha: 1)
+  static let oldGold        =       #colorLiteral(red: 0.8274509804, green: 0.7176470588, blue: 0.2705882353, alpha: 1)
+  static let mountainMeadow =       #colorLiteral(red: 0.1137254902, green: 0.7607843137, blue: 0.5490196078, alpha: 1)
+  static let malachite      =       #colorLiteral(red: 0, green: 0.8078431373, blue: 0.3568627451, alpha: 1)
+  static let sherpaBlue     =       #colorLiteral(red: 0.003921568627, green: 0.3215686275, blue: 0.3215686275, alpha: 1)
+  static let dodgerBlue     =       #colorLiteral(red: 0.03921568627, green: 0.5176470588, blue: 1, alpha: 1)
 }
 
 public extension Font {
